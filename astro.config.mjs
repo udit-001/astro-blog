fc1b213a9d6399a50e4b219abfa44c2aa25b7ede
Udit <23015406+udit-001@users.noreply.github.com>
2026-01-03 04:49:05 +0530

    feat: add cloudflare support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,22 @@ import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-micro.vercel.app",
   integrations: [sitemap(), mdx(), pagefind()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       theme: "css-variables",
     },
   },
+
+  adapter: cloudflare(),
 });
