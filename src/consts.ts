d02e350e915f4354daef238709a3365cfb6e158f
Udit <23015406+udit-001@users.noreply.github.com>
2026-01-03 04:33:46 +0530

    feat: update blog title

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
+  TITLE: "Udit's Blog",
   DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
   EMAIL: "trevortylerlee@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
