99ddddfb6dd5e5f6628401f34951ec4dada2237d
Udit <23015406+udit-001@users.noreply.github.com>
2026-01-03 07:20:35 +0530

    feat: integrate astro-og-canvas

---
+++ src/pages/og/[...id].ts
@@ -0,0 +1,18 @@
+import { getCollection } from 'astro:content';
+import { OGImageRoute } from 'astro-og-canvas';
+
+const collectionEntries = await getCollection('blog');
+
+const pages = Object.fromEntries(collectionEntries.map(({ id, data }) => [id, data]));
+
+export const { getStaticPaths, GET } = OGImageRoute({
+  param: 'id',
+  pages: pages,
+
+  getImageOptions: (path, page) => {
+    return {
+      title: page.title,
+      description: page.description
+    }
+  },
+});
