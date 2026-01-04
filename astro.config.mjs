9148f4c8583863bb4ac28cfcdbb01b606bf6b94b
Udit <23015406+udit-001@users.noreply.github.com>
2026-01-05 00:51:42 +0530

    feat: replace decap with sveltia

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,9 @@ const site =
 export default defineConfig({
   site,
 
-  integrations: [sitemap(), mdx(), pagefind(), metaTags(), decapCmsOauth()],
+  integrations: [sitemap(), mdx(), pagefind(), metaTags(), decapCmsOauth({
+      decapCMSSrcUrl: "https://unpkg.com/@sveltia/cms/dist/sveltia-cms.js",
+  })],
 
   vite: {
     plugins: [tailwindcss()],
